034373321f14123786ca2a02fe1baef17374ac02
TR0D30T <44397261+TR0D30T@users.noreply.github.com>
2020-05-19 14:36:32 +0200

    Update app.js

--- Game app/js/app.js
+++ Game app/js/app.js
@@ -1,4 +1,3 @@
-//Variables 
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const buttonReset = document.querySelector('.btn__reset');
@@ -6,11 +5,11 @@ const overlay = document.getElementById('overlay');
 const div = document.querySelector('.start');
 
 
-//Variables to keep score's track
+
 const tries = document.querySelectorAll('.tries');
 let missed = 0;
 
-//Array of phrases
+
 const arrText = [
     'hans zimmer',
     'no time for caution',
@@ -51,47 +50,35 @@ const phraseArray = getRandomPhraseAsArray(arrText);
 
 addPhraseToDisplay(phraseArray);
 
-//console.log(phraseArray);
-
-//check letter function
 const checkLetter = button => {
-    //Create a variable to store if a match is found and give it an initial value of null
     let match = null;
-
-    //get all the elements with the class letter
     const liItems = document.getElementsByClassName('letter');
-    //The function should loop over the letters and check 
-    //if they match the letter in the button the player has chosen.
     for (let i = 0; i < liItems.length; i++) {
         if (liItems[i].textContent === button) {
             liItems[i].classList.add('show');
             match = liItems[i].textContent;
         } 
-        
     }
     return match;
 };
 
-//Add an event listener to the keyboard
 qwerty.addEventListener('click', (event) => {
     const letterFound = checkLetter(event.target.innerHTML);
 
-    //if button selected add class chosen  and disable it.
     if (event.target.tagName === 'BUTTON') {
         event.target.classList.add("chosen");
         event.target.disabled = true;
     }
 
     if (letterFound === null && event.target.tagName === "BUTTON") { 
-        const lostHeart = document.getElementsByTagName('img')[missed]; //use [missed] as the index
-        lostHeart.src = 'images/lostHeart.png'; // Replace old img for new one
-        missed += 1; // add +1 per wrong letter to the score
+        const lostHeart = document.getElementsByTagName('img')[missed]; 
+        lostHeart.src = 'images/lostHeart.png';
+        missed += 1;
 
     }
     checkWin();
 })
 
-//Function checkWin
 
 const checkWin = () => {
     const letter = document.getElementsByClassName('letter');
